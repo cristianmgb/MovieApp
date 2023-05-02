18bae4d78ed4c5bb12cfcdd1cac4a78feabcdea4
Cristian Gonzalez <cristianmgb21@gmail.com>
2023-05-02 18:37:27 -0500

    Pages

---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import SimpleTemplate from '../templates/SimpleTemplate';
+
+const Home = () => {
+  return (
+    <SimpleTemplate>
+      <Text>Home</Text>
+    </SimpleTemplate>
+  );
+};
+
+export default Home;
---
+++ src/components/pages/WatchList.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import SimpleTemplate from '../templates/SimpleTemplate';
+
+const WatchList = () => {
+  return (
+    <SimpleTemplate>
+      <Text>WatchList</Text>
+    </SimpleTemplate>
+  );
+};
+
+export default WatchList;
