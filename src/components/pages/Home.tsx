b21884949f26f88554ec5213b67466607d5ba30a
Cristian Gonzalez <cristianmgb21@gmail.com>
2023-05-03 15:03:03 -0500

    call hook to get movies

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text} from 'react-native';
 import SimpleTemplate from '../templates/SimpleTemplate';
+import {useMovies} from '../../hooks/useMovies';
 
 const Home = () => {
+  const getMoviesDB = useMovies();
+
+  useEffect(() => {
+    const getMovie = async () => {
+      await getMoviesDB();
+    };
+    getMovie();
+  }, [getMoviesDB]);
+
   return (
     <SimpleTemplate>
       <Text>Home</Text>
