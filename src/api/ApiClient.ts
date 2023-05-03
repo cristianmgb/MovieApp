2941f78c4611af0a53ed0c98d3f636ffd83fd899
Cristian Gonzalez <cristianmgb21@gmail.com>
2023-05-03 15:00:32 -0500

    add api method

---
+++ src/api/ApiClient.ts
@@ -0,0 +1,25 @@
+export const BASE_URL = 'https://api.themoviedb.org/3';
+export const MOVIE = '/movie';
+export const SEARCH = '/search';
+export const CAST = '/credits';
+export const REVIEWS = '/reviews';
+export const POPULAR = '/popular';
+export const NOW_PLAYING = '/now_playing';
+export const UPCOMING = '/upcoming';
+export const TOP_RATED = '/top_rated';
+
+const API_KEY = '?api_key=9ab54deb09e30c7f062f23fb2967a540';
+
+class ApiClient {
+  async get(url: string, query: string = '') {
+    try {
+      const response = await fetch(`${url}${API_KEY}${query}`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+export default new ApiClient();
